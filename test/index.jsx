fbe644bd1328aceaf82d76216c5de6d491e10f60
Julian Grinblat <julian@dotcore.co.il>
2021-11-11 13:20:22 +0900

    Upgrade eslint and eslint-config-airbnb

--- test/index.jsx
+++ test/index.jsx
@@ -69,11 +69,14 @@ describe('react-stay-scrolled', () => {
 
   function render(element, container, cb = noop) {
     act(() => {
-      ReactDOM.render((
-        <StrictMode>
-          {element}
-        </StrictMode>
-      ), container, cb);
+      ReactDOM.render(
+        (
+          <StrictMode>
+            {element}
+          </StrictMode>
+        ), container,
+        cb,
+      );
     });
   }
 
